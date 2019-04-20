ac30d7def3407c02618607dbe5c4636f18949166
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2019-04-20 11:05:31 +0200

    Add solution.

---
+++ Source/Extension/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace HansKindberg.VisualStudio.Extensions.TextFormatter
+{
+	[Guid("06d0031e-fa4d-4110-9322-1ef1c6f2eded")]
+	public class Settings : DialogPage
+	{
+		#region Properties
+
+		[Category("General")]
+		[DefaultValue(true)]
+		[Description("Enables loading tasks into Task Runner Explorer. Restart solution required.")]
+		[DisplayName("Enable Task Runner")]
+		public bool EnableTaskRunnerExplorer { get; set; } = true;
+
+		#endregion
+	}
+}
---
+++ Source/Extension/source.extension.cs
@@ -0,0 +1,15 @@
+﻿namespace HansKindberg.VisualStudio.Extensions.TextFormatter
+{
+	public class Global
+	{
+		#region Fields
+
+		public const string PackageName = "Hans Kindberg - Text formatter";
+		public const short PackageNameResourceId = 1;
+		public const string Settings = "Settings";
+		public const short SettingsDescriptionResourceId = 3;
+		public const short SettingsResourceId = 2;
+
+		#endregion
+	}
+}
