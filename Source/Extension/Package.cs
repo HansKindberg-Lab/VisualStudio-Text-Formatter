554659b7cd4c2d376059095c1ede214cca7db601
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2019-04-21 08:27:32 +0200

    Removed a guid-attribute

--- Source/Extension/Package.cs
+++ Source/Extension/Package.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
 namespace HansKindberg.VisualStudio.Extensions.TextFormatter
 {
-	[Guid("0b70c899-e4b4-4ad4-bc58-f9cfe3cda61d")]
 	[PackageRegistration(AllowsBackgroundLoading = true, UseManagedResourcesOnly = true)]
 	[ProvideOptionPage(typeof(Settings), Global.PackageName, Global.Settings, Global.PackageNameResourceId, Global.SettingsResourceId, true)]
 	[ProvideProfile(typeof(Settings), Global.PackageName, Global.Settings, Global.PackageNameResourceId, Global.PackageNameResourceId, false, DescriptionResourceID = Global.SettingsDescriptionResourceId)]
